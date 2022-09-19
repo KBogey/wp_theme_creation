ffa341bd1510df71916e6a5cbb1e6b05bbdf24b4
Greg Desplaces <ontact@gregdesplaces.com>
2022-09-19 13:06:38 +0200

    clean code and use bootstrap CDN

--- functions.php
+++ functions.php
@@ -12,18 +12,12 @@ function ch_load_styles_and_scripts(): void
     $theme = wp_get_theme();
 
     // Stylesheets
-    wp_register_style('bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/css/bootstrap.css', '5.2.1');
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css', '5.2.1');
     wp_register_style( 'choucroute', get_template_directory_uri() . '/style.css', array('bootstrap'), $theme->get( 'Version' ) );
     wp_enqueue_style( 'choucroute' );
 
     // Scripts
-    wp_register_script(
-        'bootstrap',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js',
-        array(),
-        '5.2.1',
-        true
-    );
+    wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js', array(), '5.2.1', true);
     wp_register_script('choucroute', get_template_directory_uri() . '/src/assets/scripts/script.js', array('jquery', 'bootstrap'), $theme->get('Version'), true);
         wp_enqueue_script('choucroute');
 }
