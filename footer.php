0644a0ba3669a4538b2b2c5f998659c9af212ebf
Greg Desplaces <ontact@gregdesplaces.com>
2022-09-19 13:02:49 +0200

    clean code and use bootstrap CDN

--- footer.php
+++ footer.php
@@ -1,12 +1,17 @@
 </main>
 
-<footer>
-    <div class="container-fluid bg-dark text-light text-center">
-        <p>
-            <?php bloginfo('title'); ?>
-            &copy; 2022
-            <?php _e('All rights reserved', 'choucroute') ?>
-        </p>
+<footer class="container-fluid bg-dark text-light">
+    <div class="row  py-3">
+        <div class="col-md-12">
+            <p class="text-center small mb-0 text-secondary">
+                <a class="text-light text-decoration-none" href="mailto:<?php echo bloginfo('admin_email'); ?>"><?php bloginfo('name'); ?></a>
+                &copy;<?php echo date('Y'); ?>
+                <?= __("All rights reserved.", 'greg') ?>
+            </p>
+        </div>
     </div>
-    <?php wp_footer(); ?>
 </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
