3abe731fb47d6cbafceb2a42fb7ff5fa7989d6bd
Greg Desplaces <ontact@gregdesplaces.com>
2022-09-20 18:49:09 +0200

    Clean comment_form args

--- comments.php
+++ comments.php
@@ -10,22 +10,20 @@
 
     $current_user = wp_get_current_user();
     $textarea_placeholder = __('Your comment*', 'choucroute');
+    $require_info =  __( '* Required fields', 'choucroute' );
+    $logout_link = wp_logout_url( get_permalink() );
+    $profile_link = admin_url('profile.php');
+    $logged_in_as_text_content = sprintf(__( 'Logged in as <a href=%1$s>%2$s</a>. <a href=%3$s title=<?>Log out?</a>. %4$s','choucroute' ), $profile_link, $current_user->user_login , $logout_link, $require_info);
 
     comment_form(array(
         'title_reply' => __('Leave a Comment', 'choucroute'),
         'comment_field' =>
-            "<div class='form-group'>
+            "<div class='form-group'>x²x²²
                 <label for='comment' class='d-none'>$textarea_placeholder</label>
                 <textarea id='comment' class='form-control' name='comment' cols='45' rows='8' aria-required='true' placeholder='$textarea_placeholder'></textarea>
             </div>",
-        'comment_notes_after' => '<div class="text-small text-end">' . __( '* Required fields', 'choucroute' ) . '</div>',
-        'logged_in_as' =>
-            '<p class=logged-in-as>'
-            . sprintf(__( 'Logged in as <a href=%1$s>%2$s</a>. <a href=%3$s title=<?>Log out?</a>',
-                'choucroute' ),
-                admin_url( 'profile.php' ),
-                $current_user->user_login ,
-                wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) ) ) . '</p>',
+        'comment_notes_before' => "<div class='text-small text-end'>$require_info</div>",
+        'logged_in_as' =>"<p class=logged-in-as>$logged_in_as_text_content</p>"
     ));
     ?>
 </div>
